d8a91fd8cc4004b2dc2dfa0be9e01d0924042c71
hiepleIEC <hieplm@ieccorp.vn>
2022-11-25 12:32:06 +0700

    complete setup migrate

--- migrate-idea-kanboard.js
+++ migrate-idea-kanboard.js
@@ -19,93 +19,128 @@ require('dotenv').config({})
       return acc
     }, {})
     const allTasks = allTaskClosed.concat(allTasksOpen)
-    // const record = {name: el}
-    for (let i = 0; i < allTasks.length; i++) {
-      const task = allTasks[i]
+    const batchAllTasks = batchArr(allTasks, 100)
+    for (let i = 0; i < batchAllTasks.length; i++) {
+      const batch = batchAllTasks[i]
+      await Promise.all(
+        batch.map(async (task) => {
+          const allNameTask = task.title
+            .split(/[()]/)
+            .map((el) => el.trim())
+            .filter((el) => el)
+          const rootNameTask = allNameTask[allNameTask.length - 1]
+          const column = objColumns[task.column_id]
+          const status = task.is_active
+          const description = task.description
+          const user_kanboard_id = task.owner_id
+          const record = {
+            name: rootNameTask || '',
+            column,
+            status,
+            description,
+            user_kanboard_id,
+          }
+          // insert task => info_ideas
+          const sqlInsertIdeaInfo =
+            'INSERT INTO `ideas`.`info_ideas` (`name`, `column`, `status`, `description`, `user_kanboard_id`) VALUES ?;'
+          const resInsertIdeaInfo = await query(sqlInsertIdeaInfo, [
+            [Object.values(record)],
+          ])
+          const idea_id = resInsertIdeaInfo.insertId
 
-      const allNameTask = task.title
-        .split(/[()]/)
-        .map((el) => el.trim())
-        .filter((el) => el)
-      const rootNameTask = allNameTask[allNameTask.length - 1]
-      const column = objColumns[task.column_id]
-      const status = task.is_active
-      const description = task.description
-      const user_kanboard_id = task.owner_id
-      const record = {
-        name: rootNameTask || '',
-        column,
-        status,
-        description,
-        user_kanboard_id,
-      }
-      // insert task => info_ideas
-      const sqlInsertIdeaInfo =
-        'INSERT INTO `ideas`.`info_ideas` (`name`, `column`, `status`, `description`, `user_kanboard_id`) VALUES ?;'
-      await query(sqlInsertIdeaInfo, [[Object.values(record)]])
-      const sqlGetLastIdInserted = 'SELECT LAST_INSERT_ID();'
-      const res = await query(sqlGetLastIdInserted)
-      const idea_id = res[0]['LAST_INSERT_ID()']
-
-      // insert child name
-      if (allNameTask.length > 1) {
-        console.log(allNameTask)
-        allNameTask.pop()
-        const primaryName = allNameTask[0]
-        // insert child primary name
-        await query(
-          'INSERT INTO `ideas`.`name_ideas` (`name`, `root_id`, `is_primary`) VALUES ?;',
-          [
-            [
-              Object.values({
-                name: primaryName,
-                root_id: idea_id,
-                is_primary: 1,
-              }),
-            ],
-          ]
-        )
-        // insert child not primary name
-        allNameTask.shift()
-        for (let i = 0; i < allNameTask.length; i++) {
-          await query(
-            'INSERT INTO `ideas`.`name_ideas` (`name`, `root_id`, `is_primary`) VALUES ?;',
-            [
+          // insert child name
+          if (allNameTask.length > 1) {
+            allNameTask.pop()
+            const primaryName = allNameTask[0]
+            // insert child primary name
+            await query(
+              'INSERT INTO `ideas`.`name_ideas` (`name`, `root_id`, `is_primary`) VALUES ?;',
               [
-                Object.values({
-                  name: allNameTask[i],
-                  root_id: idea_id,
-                  is_primary: 0,
-                }),
-              ],
-            ]
+                [
+                  Object.values({
+                    name: primaryName,
+                    root_id: idea_id,
+                    is_primary: 1,
+                  }),
+                ],
+              ]
+            )
+            // insert child not primary name
+            allNameTask.shift()
+            for (let i = 0; i < allNameTask.length; i++) {
+              await query(
+                'INSERT INTO `ideas`.`name_ideas` (`name`, `root_id`, `is_primary`) VALUES ?;',
+                [
+                  [
+                    Object.values({
+                      name: allNameTask[i],
+                      root_id: idea_id,
+                      is_primary: 0,
+                    }),
+                  ],
+                ]
+              )
+            }
+          }
+          // insert external task link
+          const listExternalTaskLink = await requestKanboard(
+            'getAllExternalTaskLinks',
+            [task.id]
           )
-        }
-      }
-      // console.log({
-      //   column,
-      //   rootNameTask,
-      //   status,
-      //   description,
-      //   user_kanboard_id,
-      // })
-
-      // return id
-
-      // insert into name
+          if (listExternalTaskLink.length > 0) {
+            await Promise.all(
+              listExternalTaskLink.map((el) => {
+                return query(
+                  'INSERT INTO `ideas`.`external_link_ideas` (`root_id`, `link`) VALUES ?;',
+                  [
+                    [
+                      Object.values({
+                        root_id: idea_id,
+                        link: el.url,
+                      }),
+                    ],
+                  ]
+                )
+              })
+            )
+          }
+          // insert tag
+          const objTaskTags = await requestKanboard('getTaskTags', [task.id])
+          const listTaskTagsEntries = Object.entries(objTaskTags)
+          if (listTaskTagsEntries.length > 0) {
+            await Promise.all(
+              listTaskTagsEntries.map((el) => {
+                return query(
+                  'INSERT INTO `ideas`.`tag_ideas` (`tag_kanboard_id`, `root_id`,`tag_kanboard_name`) VALUES ?;',
+                  [
+                    [
+                      Object.values({
+                        tag_kanboard_id: el[0],
+                        root_id: idea_id,
+                        tag_kanboard_name: el[1],
+                      }),
+                    ],
+                  ]
+                )
+              })
+            )
+          }
+        })
+      )
     }
-    // console.log(allTasksOpen)
-    // const allTaskWithName = allTasksOpen.concat(allTaskClosed).map((el) => ({
-    //   id: el.id,
-    //   title: el.title,
-    //   listName: el.title
-    //     .split(/[()]/)
-    //     .map((el) => el.trim())
-    //     .filter((el) => el),
-    // }))
   } catch (error) {
     console.log('Loi', error)
   }
 
   // const getAllTaskIdeaProject =
 })()
+
+const batchArr = (arr, length) => {
+  const res = []
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(res[Math.floor(i / length)])) {
+      res[Math.floor(i / length)].push(arr[i])
+    } else res[Math.floor(i / length)] = [arr[i]]
+  }
+  return res
+}
